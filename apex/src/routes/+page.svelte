ccc034443002be02ec6c0b813e6fbc46f48a53d8
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-12-24 15:44:34 +0530

    corrected section

--- apex/src/routes/+page.svelte
+++ apex/src/routes/+page.svelte
@@ -8,8 +8,8 @@
         </div>
     </hero>
     <div class="w-full bg-zinc-800 flex justify-center">
-        <div class="max-w-screen-xl p-5 justify-center md:w-3/4">
-            <h2 class="text-white font-bold md:text-5xl text-3xl pt-20 pb-10">Welcome to Apex Research and Scientific Writing Academy</h2>
+        <div class="max-w-screen-xl p-5 py-20 justify-center md:w-3/4">
+            <h2 class="text-white font-bold md:text-5xl text-3xl pb-10">Welcome to Apex Research and Scientific Writing Academy</h2>
             <button class="bg-transparant border border-white text-neutral-100 p-3 w-40 self-start hover:bg-white hover:text-zinc-800">Explore Programs</button>
         </div>
     </div>
