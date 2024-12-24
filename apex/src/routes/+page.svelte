9670890ee9edfcda6445f383824c743fb0bc004f
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-12-24 19:20:36 +0530

    img change

--- apex/src/routes/+page.svelte
+++ apex/src/routes/+page.svelte
@@ -1,25 +1,26 @@
 <div class="w-full flex flex-col justify-center items-center">
     <hero class="flex max-w-screen-xl sm:flex-row flex-col items-center">
         <div class="sm:w-1/2 p-5">
-            <img src="/home/main_img.png" class="" alt="Person writing in paper with feather" />
+            <img src="/home/main_img.png" class="object-cover max-h-[400px] w-full" alt="Person writing in paper with feather" />
         </div>
         <div class="sm:p-5 p-2 pb-5 flex flex-col sm:w-1/2 justify-center">
             <p class="self-center sm:text-start text-center lg:text-6xl text-3xl font-bold">Empowering Researcher Through Knownledge</p>
         </div>
     </hero>
-    <div class="w-full bg-zinc-800 flex justify-center">
+    <section class="w-full bg-zinc-800 flex justify-center">
         <div class="max-w-screen-xl p-5 py-20 justify-center md:w-3/4">
             <h2 class="text-white font-bold md:text-5xl text-3xl pb-10">Welcome to Apex Research and Scientific Writing Academy</h2>
             <button class="bg-transparant border border-white text-neutral-100 p-3 w-40 self-start hover:bg-white hover:text-zinc-800">Explore Programs</button>
         </div>
-    </div>
-    <div class="flex w-full md:flex-row flex-col">
+    </section>
+    <section class="flex w-full md:flex-row flex-col">
         <div class="md:w-1/2">
-            <img src="/home/women_reading.png" class="h-full w-full " alt="Person writing in paper with feather" />
+            <img src="/home/women_reading.png" class="h-full w-full object-cover" alt="Person writing in paper with feather" />
         </div>
         <div class="sm:px-5 p-2 pb-5 flex flex-col md:w-1/2">
             <p class="text-start xl:text-5xl lg:text-3xl text-xl font-bold underline">Our Story</p>
             <p class="text-start xl:text-3xl lg:text-xl text-lg sm:px-5 pt-5"> At Apex Research and Scientific Writing Academy, we are dedicated to empowering aspiring researchers and professionals. Our mission is to provide high-quality training and resources in research methodology, scientific writing, and publication ethics. We strive to equip individuals with the skills and knowledge needed to excel in the field of scientific research.</p>
         </div>
-    </div>
+    </section>
+    
 </div>
