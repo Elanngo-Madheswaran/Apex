8beeaf91ae1a7ec6f1455a832782c7977843ef9b
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2025-05-25 20:49:07 +0530

    fix: update image paths to absolute URLs in navbar and main page

--- apex/svelte.config.js
+++ apex/svelte.config.js
@@ -11,6 +11,24 @@ export default {
         }),
         alias:{
             $components : 'src/lib/components',
-        }
+            $assets : 'src/lib/assets',
+        },
+        prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				// ignore deliberate link to shiny 404 page
+				if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+					return;
+				}
+				
+				// ignore missing image files
+				if (path.endsWith('.png') || path.endsWith('.jpg') || path.endsWith('.jpeg') || path.endsWith('.gif')) {
+					console.warn(`Warning: Asset not found: ${path}`);
+					return;
+				}
+
+				// otherwise fail the build
+				throw new Error(message);
+			}
+		}
     }
 };
