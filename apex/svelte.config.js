dd010d2e1131a516ea841e303958fcb9c6757752
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2025-05-25 21:03:23 +0530

    refactor: remove unused script and simplify svelte config

--- apex/svelte.config.js
+++ apex/svelte.config.js
@@ -1,34 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
 export default {
-    kit: {
-        adapter: adapter({
+	kit: {
+		adapter: adapter({
             pages: 'build',
             assets: 'build',
             fallback: undefined,
-            precompress: false,
-            strict: true
+            precompress: false
         }),
-        alias:{
-            $components : 'src/lib/components',
-            $assets : 'src/lib/assets',
-        },
-        prerender: {
-			handleHttpError: ({ path, referrer, message }) => {
-				// ignore deliberate link to shiny 404 page
-				if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
-					return;
-				}
-				
-				// ignore missing image files
-				if (path.endsWith('.png') || path.endsWith('.jpg') || path.endsWith('.jpeg') || path.endsWith('.gif')) {
-					console.warn(`Warning: Asset not found: ${path}`);
-					return;
-				}
-
-				// otherwise fail the build
-				throw new Error(message);
-			}
-		}
-    }
+        alias: {
+                $components: 'src/lib/components'
+        }
+	}
 };
